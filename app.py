79da0d0b138b6daefdba26539d6389b6453cbcb7
LDTpro <ninhthangnb@gmail.com>
2020-04-18 11:18:34 +0700

    :

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ def respond(sender, message):
 def is_user_message(message):
     """Check if the message is a message from the user"""
     return (message.get('message') and
-            message['message'].get('text') and
-            not message['message'].get("is_echo"))
+            message['message'].get('text'))
 
 
 @app.route("/webhook")
