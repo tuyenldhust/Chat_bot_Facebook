02dac9008395cb8aa678bae57d47e13f34940c4f
LDTpro <ninhthangnb@gmail.com>
2020-04-18 17:59:16 +0700

    Update api

--- app.py
+++ app.py
@@ -33,9 +33,8 @@ def send_message(recipient_id, text):
 
 
 def get_bot_response(message):
-    """This is just a dummy function, returning a variation of what
-    the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
+    duLieu = requests.post('https://sim.vuiz.net/post_sim.php', data={'hoi':message,'lang':'vn'})
+    return duLieu
 
 
 def verify_webhook(req):
