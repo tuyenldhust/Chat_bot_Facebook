82ac6db3699a4907ed7878e3fed026bf854c12c0
LDTpro <ninhthangnb@gmail.com>
2020-04-18 17:44:52 +0700

    Edit route flask

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def is_user_message(message):
             not message['message'].get("is_echo"))
 
 
-@app.route("/webhook")
+@app.route("/")
 def listen():
     """This is the main function flask uses to 
     listen at the `/webhook` endpoint"""
