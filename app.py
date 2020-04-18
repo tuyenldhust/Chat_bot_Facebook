3ab755a70d0e96683bf39eb04c0c6dae7c156d77
LDTpro <ninhthangnb@gmail.com>
2020-04-18 11:38:19 +0700

    Fix app.py

--- app.py
+++ app.py
@@ -6,34 +6,10 @@ FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
 VERIFY_TOKEN = 'Simsimi'# <paste your verify token here>
 PAGE_ACCESS_TOKEN = 'EAACJpmQBpRwBANaZBbCf1TMcNuLE7jC3LYT0ZCKCjVH1b5WradOiKHOJSrzZALDaOUyqFZBWC6SrZBnTuq19lGUYZB3GHKZB3VdSDZAXYOGWZChO3tmNA2g9wIo26D9X4J8dHqpz05PQTXDtMxu8Jf3fxsKiZAQrruYmcaGHuhXgIYl6mte2wCD0sh'# paste your page access token here>"
 
-def send_message(recipient_id, text):
-    """Send a response to Facebook"""
-    payload = {
-        'message': {
-            'text': text
-        },
-        'recipient': {
-            'id': recipient_id
-        },
-        'notification_type': 'regular'
-    }
-
-    auth = {
-        'access_token': PAGE_ACCESS_TOKEN
-    }
-
-    response = requests.post(
-        FB_API_URL,
-        params=auth,
-        json=payload
-    )
-
-    return response.json()
-
-def get_bot_response(message):
+def get_bot_response():
     """This is just a dummy function, returning a variation of what
     the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
+    return "Hello"
 
 
 def verify_webhook(req):
@@ -45,15 +21,40 @@ def verify_webhook(req):
 def respond(sender, message):
     """Formulate a response to the user and
     pass it on to a function that sends it."""
-    response = get_bot_response(message)
+    response = get_bot_response()
     send_message(sender, response)
 
 
 def is_user_message(message):
     """Check if the message is a message from the user"""
-    return (message.get('message') and
-            message['message'].get('text'))
+    return (message.get('message') and message['message'].get('text'))
 
+def send_message(recipient_id, text):
+    """Send a response to Facebook"""
+    payload = {
+        'message': {
+            'text': text
+        },
+        'recipient': {
+            'id': recipient_id
+        }
+    }
+
+    auth = {
+        'access_token': PAGE_ACCESS_TOKEN
+    }
+    
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(
+        FB_API_URL,
+        params=auth,
+        headers=headers,
+        json=payload
+    )
+    
+    return response.json()
 
 @app.route("/webhook")
 def listen():
